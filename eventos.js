540bb6dd68aa3c139bd479a5f5c717d6f89d1600
SoledadPadilla <soledad04padilla@gmail.com>
2024-08-14 23:14:10 -0300

    Revert "agregué css"
    This reverts commit 039b51b2fd29427e8fd465d6d96b43ba245db561.

--- eventos.js
+++ eventos.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contenedor = document.querySelector('#miBoton');
 
+    contenedor.style.padding = '5px';
+    contenedor.style.backgroundColor = 'green';
+
     contenedor.addEventListener('click', function() {
         alert('¡Hola! Soy el div');
     });
