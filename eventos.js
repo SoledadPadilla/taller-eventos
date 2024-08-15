db248dcb964c7966b150fe96cfb50953b7fe61c0
SoledadPadilla <soledad04padilla@gmail.com>
2024-08-14 22:05:10 -0300

    Update eventos.js
    estilos

--- eventos.js
+++ eventos.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contenedor = document.querySelector('boton');
-    contenedor.style.padding = '5px';
-    contenedor.style.backgroundColor: "green";
+    const contenedor = document.querySelector('#boton');
 
-    // Agregar manejador de eventos para el click en el div
+    contenedor.style.padding = '5px';
+    contenedor.style.backgroundColor = 'green';
+
     contenedor.addEventListener('click', function() {
         alert('¡Hola! Soy el div');
     });
