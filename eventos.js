039b51b2fd29427e8fd465d6d96b43ba245db561
SoledadPadilla <soledad04padilla@gmail.com>
2024-08-14 23:11:32 -0300

    agregué css

--- eventos.js
+++ eventos.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contenedor = document.querySelector('#boton');
 
-    contenedor.style.padding = '5px';
-    contenedor.style.backgroundColor = 'green';
-
     contenedor.addEventListener('click', function() {
         alert('¡Hola! Soy el div');
     });
